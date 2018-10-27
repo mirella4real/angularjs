27deee656bde0d97437bfede54a7b58aded2bac4
Mirella Batista <mbatista@icontrol.com>
2018-10-26 20:23:20 -0500

    Running unit tests

--- exercises/03_unit-testing/karma.conf.js
+++ exercises/03_unit-testing/karma.conf.js
@@ -15,6 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        "../../libs/angularjs/1.7.5/angular.js",
+        "node_modules/angular-mocks/angular-mocks.js",
+        "controller.js",
+        "controllerSpec.js"
     ],
 
 
@@ -59,7 +63,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
